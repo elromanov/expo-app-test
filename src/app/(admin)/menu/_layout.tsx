e3d2a8f323f28190c4e191ce696e8e841acfd994
Romain Chardon <romainchardon@outlook.com>
2024-02-25 22:15:03 +0100

    added admin side of app

---
+++ src/app/(admin)/menu/_layout.tsx
@@ -0,0 +1,53 @@
+import { Stack } from "expo-router";
+import { Pressable } from "react-native";
+import { Link } from "expo-router";
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+import Colors from '@/src/constants/Colors';
+
+export default function MenuStack() {
+    return <Stack screenOptions={{
+
+    }}>
+        <Stack.Screen 
+            name="index"
+            options={
+                {title: 'Menu',
+                headerRight: () => (
+                  <Link href="/" asChild>
+                    <Pressable>
+                      {({ pressed }) => (
+                        <FontAwesome
+                          name="plus-square-o"
+                          size={25}
+                          color={Colors.light.tint}
+                          style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
+                        />
+                      )}
+                    </Pressable>
+                  </Link>
+                ),}
+            }
+        />
+
+<Stack.Screen 
+            name="[id]"
+            options={
+                {title: 'Menu',
+                headerRight: () => (
+                  <Link href="/" asChild>
+                    <Pressable>
+                      {({ pressed }) => (
+                        <FontAwesome
+                          name="pencil-square-o"
+                          size={25}
+                          color={Colors.light.tint}
+                          style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
+                        />
+                      )}
+                    </Pressable>
+                  </Link>
+                ),}
+            }
+        />
+    </Stack>;
+}
